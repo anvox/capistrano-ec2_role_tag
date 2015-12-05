c426e34e1d58e9baa238358b2a53828e78a1ba6f
An Vo Nguyen Thien <an.vo@codebox.vn>
2015-12-05 19:26:33 +0700

    Add code

--- lib/capistrano-ec2_role_tag.rb
+++ lib/capistrano-ec2_role_tag.rb
@@ -2,6 +2,18 @@ require 'capistrano/ec2_role_tag/version'
 
 module Capistrano
   module Ec2RoleTag
-    
+    class << self
+      attr_accessor :provider, :stage
+      def configure
+        yield self
+        self.stage = 'staging' if self.stage.nil?
+        self.provider = AwsEC2RoleLoader.new(stage: self.stage)
+      end
+    end
+    def ec2_by_role(role)
+      Capistrano::Ec2RoleTag.provider.fetch(role)
+    end
   end
 end
+
+extend Capistrano::Ec2RoleTag
